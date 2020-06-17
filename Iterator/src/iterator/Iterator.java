83f0abcb4165e8bf53c478db32c9453bc1fe7e57
Cássio Tessaro <Cssio Tessaro@CASSIODESKTOP>
2020-06-17 19:02:11 -0300

    Remover por Iterator PROVA

---
+++ Iterator/src/iterator/Iterator.java
@@ -0,0 +1,24 @@
+package iterator;
+
+public class Iterator {
+	
+	public void removerLivro (String titulo, String autor, Data data) throws LivroInexistenteException {
+		Iterator <Livro> it = listaDeLivros.iterator();
+		boolean excecao = true;
+		while(it.hasNext()) {
+			Livro itNext = it.next();
+			if (itNext.getAutor().equals(autor) 
+					&& itNext.getTitulo().equals(titulo) 
+						&& itNext.getPublicacao().getDia() == data.getDia()
+							&& itNext.getPublicacao().getMes() == data.getMes()
+								&& itNext.getPublicacao().getAno() == data.getAno()) {
+									it.remove();
+									excecao = false;
+				}
+			}
+		if (excecao) {
+			throw new LivroInexistenteException();
+		}	
+	}
+
+}
